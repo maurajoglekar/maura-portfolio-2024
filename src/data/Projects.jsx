ac0da5969b5632d986f64bb772b10395f3850626
maurajoglekar <31258321+maurajoglekar@users.noreply.github.com>
2024-01-20 21:29:58 -0800

    add coursera portfolio

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,22 +1,32 @@
 export const Projects = [
   {
-    thumbnail: "./images/portfolio-2017-app.png",
-    title: "Portfolio 2017",
-    tags: ["Javascript", "React", "CSS", "Bootstrap"],
+    thumbnail: "./images/coursera-portfolio.png",
+    title: "Coursera Advanced React final project",
+    tags: ["React", "Javascript", "CSS", "Chakra UI", "Formik and Yup"],
     paragraph:
-      "First react portfolio",
-    codelink: "https://github.com/maurajoglekar/react-portfolio-2017/tree/master",
-    projectlink: "https://wonderful-arithmetic-982bff.netlify.app/",
+      "Final project for Coursera Advanced React course taken in January 2024.",
+    codelink: "https://github.com/maurajoglekar/coursera_my_portfolio",
+    projectlink: "https://fabulous-macaron-014ae6.netlify.app/",
     projectlinkText: "View Project",
   },
   {
     thumbnail: "./images/appointments-app.png",
     title: "Appointments application",
     tags: ["React", "Tailwind CSS"],
-    paragraph:
-      "Project built during linkedIn Learning class",
-    codelink: "https://github.com/maurajoglekar/myreact-2023/tree/appointments-app",
+    paragraph: "Project built during linkedIn Learning class",
+    codelink:
+      "https://github.com/maurajoglekar/myreact-2023/tree/appointments-app",
     projectlink: "https://spectacular-beignet-0bfdbc.netlify.app/",
+    projectlinkText: "View Project",
+  },
+  {
+    thumbnail: "./images/portfolio-2017-app.png",
+    title: "Portfolio 2017",
+    tags: ["React", "Javascript", "CSS", "Bootstrap"],
+    paragraph: "First react portfolio",
+    codelink:
+      "https://github.com/maurajoglekar/react-portfolio-2017/tree/master",
+    projectlink: "https://wonderful-arithmetic-982bff.netlify.app/",
     projectlinkText: "View Project",
   },
 ];
