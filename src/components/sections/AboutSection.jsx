feeb86c921a0e46fc5690682251e37f1b4e770dc
maurajoglekar <31258321+maurajoglekar@users.noreply.github.com>
2024-02-09 10:38:36 -0800

    updated about details

--- src/components/sections/AboutSection.jsx
+++ src/components/sections/AboutSection.jsx
@@ -1,4 +1,3 @@
-
 import {
   AboutContent,
   DarkCircle,
@@ -24,25 +23,42 @@ export const AboutSection = () => {
             Hello <StyledEmoji>👋</StyledEmoji>
           </StyledHeading>
           <p>
-            My name is Maura Joglekar.  I'm originally from Mexico, lived in Colorado during my school years and now live in California.
+            My name is Maura Joglekar. I'm originally from Mexico, lived in
+            Colorado during my school years and now live in California.
           </p>
           <p>
-          Most recently,  I worked at F5 as a principal frontend developer in two different products building them from the ground up:
+            Most recently, I worked at F5 as a principal frontend developer in
+            two different products building them from the ground up:
           </p>
           <ol>
             <li>
-              As part of the F5 Cloud services, I was a main contributor for the login flow, account management, subscription flows, services onboarding, and the billing enablement. 
+              As part of the F5 Cloud services, I was a main contributor for the
+              login flow, account management, subscription flows, services
+              onboarding, and the billing enablement. Used UI technologies
+              Javascript ES6, ReactJS, redux and styled components. Also did the
+              UI QA automation with selenium and python.
             </li>
             <li>
-              As part of the NGINX product, I was the only frontend lead in the Analytics platform team building beautiful analytics dashboards to help customers identify, diagnose and solve issues.
+              As part of the Nginx NMS product, I built beautiful analytics
+              dashboards to help customers identify, diagnose and solve issues.
+              Also I implemented the instance metrics, notifications and events
+              features using Typescript, ReactJS, and styled components. Also
+              did the UI QA automation with playwright and javascript.
             </li>
           </ol>
 
-           <p>
-             Prior to that, I worked at Oracle as a experienced developer.  I was granted a patent for one of my projects and I worked closely with their biggest customer of that product to migrate billions of documents using that patented feature. 
+          <p>
+            Prior to that, I worked at Oracle as a experienced developer. I was
+            granted a patent for one of my projects and I worked closely with
+            their biggest customer of that product to migrate billions of
+            documents using that patented feature.
           </p>
           <p>
-            I have great collaboration and communication skills that enabled me to work well with product management, quality assurance, and backend engineers. I am very passionate about ensuring a good customer experience and I take extreme pride in my work. I also enjoy mentoring junior developers and interns.
+            I have great collaboration and communication skills that enabled me
+            to work well with product management, quality assurance, and backend
+            engineers. I am very passionate about ensuring a good customer
+            experience and I take extreme pride in my work. I also enjoy
+            mentoring junior developers and interns.
           </p>
         </AboutContent>
       </StyledAboutSection>
